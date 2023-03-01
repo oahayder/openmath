32577016693ab0a3283eb089af5310e971cf382c
Omar Al-Hayderi <oahayder@gmail.com>
2023-02-28 18:44:52 -0700

    Init commit

--- main.py
+++ main.py
@@ -2,17 +2,34 @@ import os
 
 import openai
 
-def generate_assignment(age):
-    return """Write a math question for a {} year old:""".format(age)
+def train_model():
+    '''
+    Ask the user a set of questions. write a JSONL file with format:
 
-def grade_assignment(assigment):
-    # Call OpenAPI for assignment
+    {"prompt": "<prompt text>", "completion": "<ideal generated text>"}
+    {"prompt": "<prompt text>", "completion": "<ideal generated text>"}
+
+    :return:
+    '''
+
+    return null
+
+def generate_assignment(age="16", interest="sports"):
+    return """Write a math question for a {} year old that likes {}:""".format(age, interest)
+
+def call_prompt(prompt, temperature=0.7, max_tokens=4000):
+    # Call OpenAPI for result
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Grade the answers below:\n{}".format(assigment),
-        temperature=0.7,
-        max_tokens=4000,
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
     )
+
+    return response
+
+def grade_assignment(assigment):
+    response = call_prompt("Grade the answers below:\n{}".format(assigment))
 
     return response
 
@@ -20,16 +37,11 @@ def grade_assignment(assigment):
 if __name__ == '__main__':
     # Prompt for age
     age = input("Hello! Please enter your age: ")
+    interest = input("What's your favourite thing? ")
 
-    # Call OpenAPI for assignment
-    assignment_prompt = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=generate_assignment(age),
-        temperature=0.7,
-        max_tokens=4000,
-    )
 
-    #print(assignment_prompt)
+    assignment_prompt = call_prompt(generate_assignment(age, interest))
+
     assignment_completed = ""
     '''
     for line in assignment_prompt.splitlines():
